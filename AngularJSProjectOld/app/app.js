b3e7d053b890263a74956e468814b70998f2c673
Kseniya2805 <ksyusha2805@yandex.ru>
2018-11-12 08:36:39 +0300

    Deleted old projects.

--- AngularJSProjectOld/app/app.js
+++
@@ -1,8 +0,0 @@
-var riskApp = angular.module('riskApp', ['ngRoute']).config(['$routeProvider', function ($routeProvider) {
-
-    $routeProvider.when('/riskDetailsCapture', {
-        templateUrl: 'riskDetailsCapture/riskDetailsCapture.html',
-        controller: 'RiskDetailsCaptureCtrl'
-    });
-
-}]);
--- ProjectAngularJS/src/app/components/components.js
+++
@@ -1,8 +0,0 @@
-import angular from 'angular';
-    import RiskDetailsCaptureModule from './riskDetailsCapture/riskDetailsCapture.module';
-
-const ComponentsModule = angular.module('app.components',[
-       RiskDetailsCaptureModule.name 
-]);
-
-export default ComponentsModule;
